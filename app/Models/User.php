def7f00d5d1a9334b025a17bcbeb479b998b46ad
robinkamaliev <robinkamaliev@mail.ru>
2023-12-30 21:36:20 +0500

    feat: init jwt laravel + base config

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -47,5 +48,15 @@ class User extends Authenticatable
     public function address(): HasMany
     {
         return $this->hasMany(Coordinate::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(): array
+    {
+       return [];
     }
 }
